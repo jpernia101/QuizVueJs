d5965ad4ce567d00277c3b374dc20d096eae8aa1
jpernia101 <jeffreypernia@gmail.com>
2020-09-21 20:21:29 -0500

    second commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+		<b-jumbotron>
+			<template v-slot:lead>
+				{{currentQuestion.question}}
+			</template>
+
+			<hr class="my-4">
+
+			<p>
+				answer choices
+			</p>
+
+			<b-button variant="primary" href="#">Do Something</b-button>
+			<b-button variant="success" href="#">Do Something Else</b-button>
+		</b-jumbotron>
+	</div>
+</template>
+
+<script>
+export default {
+	props:{
+		currentQuestion: Object
+	}, 
+}
+</script>
